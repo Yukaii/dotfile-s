4cec649992aa602e8e7006e1bb488b0ba3255c09
Yukai Huang <yukaihuangtw@gmail.com>
2024-11-04 21:47:23 +0800

    gty: simplify

--- fish/.config/fish/completions/gty.fish
+++ fish/.config/fish/completions/gty.fish
@@ -1,5 +1,5 @@
 # Completion script for gty - CLI wrapper for ghostty with subcommands
 
 # Main subcommands
-complete -c gty -n '__fish_use_subcommand' -a "config-docs" -d "Show ghostty's default configuration documentation in TOML format" --no-files
-complete -c gty -n '__fish_use_subcommand' -a "action-docs" -d "List available actions for ghostty with detailed documentation in YAML format" --no-files
+complete -c gty -n '__fish_use_subcommand' -a "config-docs" -d "Show ghostty's default configuration documentation" --no-files
+complete -c gty -n '__fish_use_subcommand' -a "action-docs" -d "List available actions for ghostty" --no-files
