bc64963f0a91cb2f7dd7d73b3a537bd0f9141fd1
Yukai Huang <yukaihuangtw@gmail.com>
2024-11-06 21:11:52 +0800

    fish: now manage alias with atuin

---
+++ fish/.config/fish/aliases.fish
@@ -0,0 +1,42 @@
+# Backup of imported aliases
+
+alias ping='prettyping --nolegend'
+alias pg8='ping 8.8.8.8'
+alias vim='nvim'
+alias cat='bat'
+alias preview="fzf --preview 'bat --color \"always\" {}'"
+alias du="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
+alias pn="pnpm"
+alias ed="ed -p ':'"
+alias ts='sesh connect $(sesh list | fzf)'
+alias gaa="git add --all"
+alias gcount='git shortlog -sn'
+alias gcl='git config --list'
+alias gcd='git checkout-default'
+alias gcm='git checkout-master'
+alias grh='git reset HEAD'
+alias grhh='git reset HEAD --hard'
+alias gclean='git reset --hard; and git clean -dfx'
+alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
+alias gfgrep='git ls-files | grep'
+alias gpoat='git push origin --all; and git push origin --tags'
+alias gg='git grep -n'
+alias gk='gitk --all --branches'
+alias gsts='git stash show --text'
+alias gsta='git stash'
+alias gstp='git stash pop'
+alias gstd='git stash drop'
+alias grt='cd (git rev-parse --show-toplevel or echo ".")'
+alias gsr='git svn rebase'
+alias gsd='git svn dcommit'
+alias gwip='git add -A; git ls-files --deleted -z | xargs -0 git rm; git commit -m "wip"'
+alias gunwip='git log -n 1 | grep -q -c wip; and git reset HEAD~1'
+alias ptt="ssh bbsu@ptt.cc"
+alias l='ls -alh'
+alias em="emacs -nw"
+alias kbc="kubectl"
+alias ati="atuin search -i"
+alias pm2lc="pm2_app_selector logs"
+alias pm2s="pm2_app_selector start"
+alias pm2rs="pm2_app_selector restart"
+alias pm2st="pm2_app_selector stop"
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -33,17 +33,8 @@ fish_add_path /opt/homebrew/opt/make/libexec/gnubin
 set -x PIPENV_SHELL_FANCY 1
 set -x PIPENV_IGNORE_VIRTUALENVS 1
 
-# Aliases
-alias ping='prettyping --nolegend'
-alias pg8='ping 8.8.8.8'
-alias vim='nvim'
-alias cat='bat'
-alias preview="fzf --preview 'bat --color \"always\" {}'"
-alias du="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
-alias pn="pnpm"
-alias ed="ed -p ':'"
-alias ts='sesh connect $(sesh list | fzf)'
-# alias eza="ls"
+alias ibrew='arch -x86_64 /usr/local/homebrew/bin/brew'
+alias mbrew='arch -arm64e /opt/homebrew/bin/brew'
 
 # Git Aliases
 alias g='git' #compdef g=git
@@ -54,7 +45,6 @@ alias gpu='git pull' #compdef _git gl=git-pull
 alias gpur='git pull --rebase' #compdef _git gup=git-fetch
 alias gp='git push' #compdef _git gp=git-push
 alias gpf='git push --force-with-lease' #compdef _git gpf=git-push
-alias gaa="git add --all"
 alias gc='git commit -v' #compdef _git gc=git-commit
 alias gcln='git clone' #compdef _git gcln=git-clone
 alias gc!='git commit -v --amend' #compdef _git gc!=git-commit
@@ -63,8 +53,6 @@ alias gca!='git commit -v -a --amend' #compdef _git gca!=git-commit
 alias gcar='git commit -v -a --amend --reuse-message=HEAD' #compdef _git gcar!=git-commit
 alias gcmsg='git commit -m' #compdef _git gcmsg=git-commit
 alias gco='git checkout' #compdef _git gco=git-checkout
-alias gcd='git checkout-default'
-alias gcm='git checkout-master'
 alias gr='git remote' #compdef _git gr=git-remote
 alias grv='git remote -v' #compdef _git grv=git-remote
 alias grbi='git rebase -i' #compdef _git grbi=git-rebase
@@ -73,62 +61,24 @@ alias grba='git rebase --abort' #compdef _git grba=git-rebase
 alias gb='git branch' #compdef _git gb=git-branch
 alias gba='git branch -a' #compdef _git gba=git-branch
 alias gbr='git branch -r' #compdef _git gbr=git-branch-remote
-alias gcount='git shortlog -sn'
-alias gcl='git config --list'
 alias gcp='git cherry-pick' #compdef _git gcp=git-cherry-pick
 alias glg='git log --stat --max-count=10' #compdef _git glg=git-log
 alias glgg='git log --graph --max-count=10' #compdef _git glgg=git-log
 alias glgga='git log --graph --decorate --all' #compdef _git glgga=git-log
-alias gl="serie"
-alias gll='git log --oneline --pretty="format:%C(yellow)%h %Cblue%>(12)%ad %Cgreen%<(7)%aN%Cred%d %Creset%s" --date=relative --graph --branches --remotes --tags --decorate'
 alias gss='git status -s' #compdef _git gss=git-status
 alias ga='git add' #compdef _git ga=git-add
 alias gm='git merge' #compdef _git gm=git-merge
-alias grh='git reset HEAD'
-alias grhh='git reset HEAD --hard'
-alias gclean='git reset --hard; and git clean -dfx'
-alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
 alias grpr-dry='git remote prune origin --dry-run' #compdef _git grpr=git-remote-prune-origin
 alias grpr='git remote prune origin' #compdef _git grpr=git-remote-prune-origin
 alias gf='git fetch' #compdef _git gf=git-fetch
-alias gfgrep='git ls-files | grep'
-alias gpoat='git push origin --all; and git push origin --tags'
 alias gmt='git mergetool --no-prompt' #compdef _git gm=git-mergetool
-alias gg='git grep -n'
-alias gk='gitk --all --branches'
-alias gsts='git stash show --text'
-alias gsta='git stash'
-alias gstp='git stash pop'
-alias gstd='git stash drop'
-alias grt='cd (git rev-parse --show-toplevel or echo ".")'
-
-# Git and SVN mix
 alias git-svn-dcommit-push='git svn dcommit; and git push github master:svntrunk' #compdef git-svn-dcommit-push=git
-alias gsr='git svn rebase'
-alias gsd='git svn dcommit'
-
 alias ggpull='git pull origin (current_branch)' #compdef ggpull=git
 alias ggpur='git pull --rebase origin (current_branch)' #compdef ggpur=git
 alias ggpush='git push origin (current_branch)' #compdef ggpush=git
 alias ggpnp='git pull origin (current_branch); and git push origin (current_branch)' #compdef ggpnp=git
 
-# Aliases for committing and managing WIP branches
-alias gwip='git add -A; git ls-files --deleted -z | xargs -0 git rm; git commit -m "wip"'
-alias gunwip='git log -n 1 | grep -q -c wip; and git reset HEAD~1'
-
-# Miscellaneous Aliases
-alias ptt="ssh bbsu@ptt.cc"
-alias l='ls -alh'
-alias em="emacs -nw"
-alias kbc="kubectl"
-alias ati="atuin search -i"
-alias pm2lc="pm2_app_selector logs"
-alias pm2s="pm2_app_selector start"
-alias pm2rs="pm2_app_selector restart"
-alias pm2st="pm2_app_selector stop"
-alias ibrew='arch -x86_64 /usr/local/homebrew/bin/brew'
-alias mbrew='arch -arm64e /opt/homebrew/bin/brew'
-
+# Functions for various purposes
 function set_current_docker_host
   set -x DOCKER_HOST (docker context inspect --format '{{.Endpoints.docker.Host}}')
 end
